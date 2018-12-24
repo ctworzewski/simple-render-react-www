b76e88fbcdbeaa0b3ed4b354b332c0f130800329
Cezary <cezary.tworzewski@gmail.com>
2018-12-24 08:18:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-render-react-www
+
+## Tworzę elementy  `header`, `main` oraz `footer`, które służą jako moja strona internetowa, następnie wstrzykuje te elementy do DOM'a - wyniku czego powstaje prosta strona internetowa.
---
+++ app.js
@@ -0,0 +1,24 @@
+const header = <h1 className="title">Witaj na stronie!</h1>
+
+const classBig = 'big';
+const handleClick = () => {
+    alert('kliknął');
+}
+
+const footerTxt = 'Copyright 2018';
+const main = (
+    <div>
+        <h1 onClick={handleClick} className='red'>Pierwszy artykuł</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi iure qui tempore non, fugiat facilis ea distinctio, at deserunt quae voluptatem nihil deleniti vitae nostrum earum repellat accusantium ipsa unde!</p>
+    </div>
+)
+const footer = (
+<footer className={classBig}> 
+    <p>Stopka</p>
+    {footerTxt}
+</footer>
+)
+
+const app = [header, main, footer];
+
+ReactDOM.render(app, document.getElementById('root'));
